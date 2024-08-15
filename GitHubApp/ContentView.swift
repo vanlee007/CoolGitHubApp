a8f83f2c75ad0a4d3feee7d43e18516f6dd6c5f7
Van Lee <VanLee.Chiqwada@norrinradd.co>
2024-08-15 14:02:30 +0200

    Initial Commit

---
+++ GitHubApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubApp
+//
+//  Created by Van Lee Chigwada on 2024/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by Van Lee Chigwada on 2024/08/15.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
